1fb74d085948be77272d92875a28af4beef4b122
mdnrosen <mdnrosen@hotmail.co.uk>
2019-04-10 10:03:01 +0100

    category and base models filled

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
-
-
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABSE_URI'] = 'postgres://localhost:5432/itchyfeet'
--- models/category.py
+++ models/category.py
@@ -1,1 +1,11 @@
-# itchy feet
+from marshmallow import fields
+from app import db, ma
+from .base import BaseModel
+
+class Category(db.Model, BaseModel):
+    __tablename__ = 'category_table'
+
+    name = db.Column(db.String(40), unique=True, nullable=False)
+
+class CategorySchema(ma.ModelSchema):
+    trips = fields.Nested('TripSchema', many=True)
