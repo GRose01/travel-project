9c4b4d3d4b2254370fe7160b62d4e622e5ecb283
mdnrosen <mdnrosen@hotmail.co.uk>
2019-04-10 09:39:35 +0100

    app.py written

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-# itchy feet
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+
+
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABSE_URI'] = 'postgres://localhost:5432/itchyfeet'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
