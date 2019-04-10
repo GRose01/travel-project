d546474a6aa8327c4635456db30e2461fdccd927
mdnrosen <mdnrosen@hotmail.co.uk>
2019-04-10 14:40:43 +0100

    added crud methods to trips

--- controllers/trips.py
+++ controllers/trips.py
@@ -1,13 +1,57 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, request, g
 from models.trip import Trip, TripSchema
 from models.category import Category
+from lib.secure_route import secure_route
+
 
 trip_schema = TripSchema()
 
 api = Blueprint('trips', __name__)
 
 # INDEX ROUTE
-api.route('/trips', methods=['GET'])
+@api.route('/trips', methods=['GET'])
 def index():
     trips = Trip.query.all()
     return trip_schema.jsonify(trips, many=True), 200
+
+
+@api.route('/trips/<int:trip_id>', methods=['GET'])
+def show(trip_id):
+    trip = Trip.query.get(trip_id)
+    return trip_schema.jsonify(trip), 200
+
+
+@api.route('/trips', methods=['POST'])
+@secure_route
+def create():
+    trip, errors = trip_schema.load(request.get_json())
+    if errors:
+        print(errors)
+        return jsonify(errors, 422)
+    trip.save()
+    return trip_schema.jsonify(trip)
+
+
+@api.route('/trips/<int:trip_id>', methods=['PUT'])
+@secure_route
+def update(trip_id):
+    trip = Trip.query.get(trip_id)
+    trip, errors = trip_schema.load(request.get_json(), instance=trip, partial=True)
+    if errors:
+        print(errors)
+        return jsonify(errors, 422)
+
+    if trip.creator != g.current_user:
+        return jsonify({'message': 'Unauthorized'}), 422
+
+    trip.save()
+    return trip_schema.jsonify(trip)
+
+@api.route('/trips/<int:trip_id>', methods=['DELETE'])
+@secure_route
+def delete(trip_id):
+    trip = Trip.query.get(trip_id)
+    if trip.creator != g.current_user:
+        return jsonify({'message': 'Unauthorized'}), 422
+    trip.remove()
+    return 'Trip deleted', 204
--- seeds.py
+++ seeds.py
@@ -23,9 +23,15 @@ with app.app_context():
 # create trips
     madrid = Trip(name='Madrid', images='url_here', description='The city that knows how to live: Spain’s capital city is very alive and has indeed a very friendly neighbourhood feel', budget=2000, number_of_days=5, time_of_year='September', categories=[])
 
+    nepal = Trip(name='Annapurna Trek', images='url_here', description='Trekking in the Himalayas, eating dahl and searching for snow leopards', budget=2500, number_of_days=30, time_of_year='April', categories=[])
+
+    iceland = Trip(name='Iceland Tour', images='url_here', description='Seeing northern lights and all the sights!', budget=1500, number_of_days=4, time_of_year='January', categories=[])
+
 
 # adding and commiting to the session
 
     db.session.add(madrid)
+    db.session.add(nepal)
+    db.session.add(iceland)
 
     db.session.commit()
