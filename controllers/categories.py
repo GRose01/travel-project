d3322f025c3227c9096c1741ffb2785c39162621
mdnrosen <mdnrosen@hotmail.co.uk>
2019-04-11 09:54:24 +0100

    set up webpack proxy and added category index route

--- controllers/categories.py
+++ controllers/categories.py
@@ -1,1 +1,10 @@
+from flask import Blueprint, jsonify
+from models.category import Category, CategorySchema
 
+category_schema = CategorySchema()
+api = Blueprint('categories', __name__)
+
+@api.route('/categories', methods=['GET'])
+def index():
+    categories = Category.query.all()
+    return category_schema.jsonify(categories, many=True), 200
