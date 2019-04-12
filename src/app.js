1c2141715722bf5cb3dd8a917f73dd69b3285a35
Gabriella Rose <gabriella.sophie.rose@gmail.com>
2019-04-12 10:55:12 +0100

    edit/create trip

--- src/app.js
+++ src/app.js
@@ -3,9 +3,16 @@ import ReactDOM from 'react-dom'
 
 import './style.scss'
 
+import { BrowserRouter as Browser, Route, Switch } from 'react-router-dom'
+
+import Home from './components/common/home'
 import Header from './components/common/header'
 import Footer from './components/common/footer'
-import Home from './components/common/home'
+import Register from './components/user/register'
+import Login from './components/user/login'
+import ViewTrip from './components/trips/viewtrip'
+import EditTrip from './components/trips/edittrip'
+import CreateTrip from './components/trips/createtrip'
 
 class App extends React.Component {
   constructor() {
@@ -18,11 +25,20 @@ class App extends React.Component {
 
   render() {
     return(
-      <div>
-        < Header />
-        < Home />
-        < Footer />
-      </div>
+      <Browser>
+        <main>
+          <Header />
+          <Switch>
+            <Route path="/register" component={Register} />
+            <Route path="/login" component={Login} />
+            <Route path="/trips/new" component={CreateTrip} />
+            <Route path="/trips/:id/edit" component={EditTrip} />
+            <Route path="/trips/:id/" component={ViewTrip} />
+            <Route exact path="/" component={Home} />
+          </Switch>
+          <Footer />
+        </main>
+      </Browser>
     )
   }
 }
