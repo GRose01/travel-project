0ea449c05434824f9c772cc3da684cd1a530edda
mdnrosen <mdnrosen@hotmail.co.uk>
2019-04-12 11:16:58 +0100

    try again

---
+++ src/components/pullables/submitbutton.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+class SubmitButton extends React.Component {
+  constructor(){
+    super()
+
+  }
+
+  render(){
+    return(
+      <button className="submitButton">
+        <i className="fas fa-plane-departure"></i>
+      </button>
+    )
+  }
+}
+
+export default SubmitButton
