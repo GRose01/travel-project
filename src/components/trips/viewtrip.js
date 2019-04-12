f0753a98ca49f91648ffe697d3e1037b3b3c1061
Gabriella Rose <gabriella.sophie.rose@gmail.com>
2019-04-12 10:14:37 +0100

    view trip page added

--- src/components/trips/viewtrip.js
+++ src/components/trips/viewtrip.js
@@ -6,48 +6,53 @@ class ViewTrip extends React.Component {
     super()
 
     this.state = {}
+  }
 
-    componentDidMount() {
-      axios.get(`/api/trips/${this.props.match.params.id}`)
-        .then(res => this.setState({ trip: res.data }))
-    }
+  componentDidMount() {
+    axios.get(`/api/trips/${this.props.match.params.id}`)
+      .then(res => this.setState({ trip: res.data }))
+  }
 
-    render() {
-      return (
-        <main>
-          <div className="contains-title_photo">
-            <div className="title">
-              <h1> </h1>
-            </div>
-            <div className="budget">
-              <h4> £100 </h4>
-            </div>
-            <div className="duration">
-              <h4> 5 days </h4>
-            </div>
+  render() {
+    const { trip } = this.state
+    return (
+      <main>
+        <div className="contains-title_photo">
+          <div className="title">
+            <h1>Destination {trip.name}</h1>
           </div>
+          <div className="budget">
+            <h4> Budget {trip.budget}</h4>
+          </div>
+          <div className="duration">
+            <h4> Duration {trip.number_of_days}</h4>
+          </div>
+        </div>
 
-            <div className="showPhotos">
-              <img />
-              <img />
-              <img />
-              <img />
-            </div>
-          </div>
+        <div className="showPhotos">
+          <img />
+          <img />
+          <img />
+          <img />
+        </div>
 
-          <div className="contains-categories">
-            <h4>categories</h4>
-          </div>
-          <div className="contains-like_viewTrip">
-            <h4>  </h4>
-            <button></button>
-            <i className="far fa-thumbs-up"></i>
-          </div>
-          <div data-skyscanner-widget="SearchWidget"></div>
-          <script src="https://widgets.skyscanner.net/widget-server/js/loader.js" async></script>
-        </main>
-      )
-    }
+        <div className="recommendations">
+          <h1>Recommendations {trip.description}</h1>
+        </div>
+
+        <div className="map">
+          <h1>Mapbox api</h1>
+        </div>
+
+        <div className="categories">
+          <h1>Categories {trip.categories}</h1>
+        </div>
+
+        <div className="flights">
+          <h1>Flight widget</h1>
+        </div>
+      </main>
+    )
   }
 }
 
