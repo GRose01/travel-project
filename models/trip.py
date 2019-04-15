a8f5f6e73c4712b6a8dd6775ba7a5a9fc5e0c111
mdnrosen <mdnrosen@hotmail.co.uk>
2019-04-15 10:19:10 +0100

    fixed seeds

--- models/trip.py
+++ models/trip.py
@@ -3,6 +3,8 @@ from marshmallow import fields
 from .base import BaseModel
 from .category import Category
 from .user import User
+from .duration import Duration
+from .budget import Budget
 
 likes = db.Table(
     'likes',
@@ -21,13 +23,14 @@ class Trip(db.Model, BaseModel):
     __tablename__ = 'trips_table'
 
     name = db.Column(db.String(50), nullable=False)
-    images = db.Column(db.String(128))
+    images = db.Column(db.String(500))
     description = db.Column(db.String(128), nullable=False)
-    budget = db.Column(db.Integer, nullable=False)
-    number_of_days = db.Column(db.Integer, nullable=False)
-    time_of_year = db.Column(db.String(20), nullable=False)
-    categories = db.relationship('Category', secondary=trips_categories, backref='trips')
     creator_id = db.Column(db.Integer, db.ForeignKey('user_table.id'))
+    budget = db.relationship('Budget')
+    budget_id = db.Column(db.Integer, db.ForeignKey('budget_table.id'))
+    duration = db.relationship('Duration')
+    duration_id = db.Column(db.Integer, db.ForeignKey('duration_table.id'))
+    categories = db.relationship('Category', secondary=trips_categories, backref='trips')
     creator = db.relationship('User', backref='created_trip')
     liked_by = db.relationship('User', secondary=likes, backref='likes')
 
@@ -35,6 +38,8 @@ class TripSchema(ma.ModelSchema):
     categories = fields.Nested('CategorySchema', many=True, only=('name',))
     creator = fields.Nested('UserSchema', only=('id', 'username'))
     liked_by = fields.Nested('UserSchema', many=True, only=('id', 'username'))
+    budget = fields.Nested('BudgetSchema', only='cost')
+    duration = fields.Nested('DurationSchema', only='duration')
 
     class Meta:
         model = Trip
