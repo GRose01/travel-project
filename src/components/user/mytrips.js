35f6c305a997c7566e5bf7bc660d4da4db72772c
Gabriella Rose <gabriella.sophie.rose@gmail.com>
2019-04-15 23:16:25 +0100

    my trips page working

--- src/components/user/mytrips.js
+++ src/components/user/mytrips.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import axios from 'axios'
+import { Link } from 'react-router-dom'
 import Auth from '../../lib/auth'
-const moment = require('moment')
 
 class MyTrips extends React.Component{
   constructor(){
@@ -12,65 +12,72 @@ class MyTrips extends React.Component{
     }
   }
 
+  // componentDidMount() {
+  //   axios.all([
+  //     axios.get('/api/user', {
+  //       headers: { Authorization: `Bearer ${Auth.getToken()}`}
+  //     }),
+  //     axios.get('/api/trips')
+  //   ])
+  //     .then(res => {
+  //       const [user, tripsOne] = res
+  //       const trips = tripsOne.data.filter(trip => {
+  //         return trip.creator.id === user.id
+  //       })
+  //       this.setState({ trips, user: user.data })
+  //     })
+  //     .catch(err => console.log(err))
+  // }
+
   componentDidMount() {
-    axios.all([
-      axios.get('/api/user', {
-        headers: { Authorization: `Bearer ${Auth.getToken()}`}
-      }),
-      axios.get('/api/trips')
-    ])
+    axios.get('/api/user', {
+      headers: { Authorization: `Bearer ${Auth.getToken()}`}
+    })
       .then(res => {
-        const [user, tripsOne] = res
-        const trips = tripsOne.data.filter(trip => {
-          return trip.creator.id === user.id
-        })
-        this.setState({ trips, user: user.data })
+        this.setState({ myTrips: res.data.created_trips })
       })
       .catch(err => console.log(err))
   }
 
 
   render(){
-    console.log(this.state)
-    if (!this.state.trips.length) return null
+    console.log(this.state.myTrips, '<-- trips')
+    // if (!this.state.trips) return null
     return(
       <main>
-        {!this.state.trips && <p> Finding trips ...</p>}
-        {this.state.trips && this.state.trips.map(trip => (
+        {!this.state.myTrips && <p> Finding trips ...</p>}
+        {this.state.myTrips && this.state.myTrips.map(trip => (
 
           <div key={trip.id} className="trip-card">
-            <div className="contains-title_photo">
-              <div className="title">
-                <h1>{trip.name}</h1>
+            <Link to={`/trips/${trip.id}`} >
+              <div className="contains-title_photo">
+                <div className="title">
+                  <h1>{trip.name}</h1>
+                </div>
               </div>
-              <div className="description">
-                <p>{trip.description}</p>
+
+              <div className="">
+                <img src={trip.images} alt={trip.name}/>
               </div>
-            </div>
 
-            <div className="contains-budget_duration">
-              <div className="budget">
-
-                <h4>{trip.budget}</h4>
+              <div className="contains-budget_duration">
+                <div className="budget">
+                  <h4>{trip.budget}</h4>
+                </div>
+                <div className="duration">
+                  <h4>{trip.duration.duration}</h4>
+                </div>
               </div>
-              <div className="duration">
-                <h4>{trip.duration.duration}</h4>
+
+              <div className="contains-categories">
+                <h4>{trip.categories.map((category, i) => (
+                  <span key={i}>{category.name}, </span>))}</h4>
               </div>
-            </div>
+              <div className="contains-like_viewTrip">
+                <h4>{trip.liked_by.length} likes</h4>
+              </div>
 
-            <div className="contains-categories">
-              <h4>{trip.categories.map((category, i) => (
-                <span key={i}>{category.name}, </span>))}</h4>
-            </div>
-            <div className="contains-like_viewTrip">
-              <h4>{trip.liked_by.length} likes</h4>
-              <button>VIEW</button>
-              <i className="far fa-thumbs-up"></i>
-            </div>
-
-            <div className="card-content">
-              <h6>Created by {trip.creator.username} <br/> at {moment(trip.createdAt).format('hh:mm')} on {moment(trip.createdAt).format('Do MMMM YYYY')}</h6>
-            </div>
+            </Link>
           </div>
         ))}
       </main>
