79c283de28c0eb7d2ec71d955154aadf56d5a5a0
mdnrosen <mdnrosen@hotmail.co.uk>
2019-04-16 19:24:23 +0100

    final styling complete

--- src/components/user/mytrips.js
+++ src/components/user/mytrips.js
@@ -19,7 +19,6 @@ class MyTrips extends React.Component{
     })
       .then(res => {
         this.setState({ myTrips: res.data.created_trips })
-        console.log(this.state.myTrips[0].id, 'THIS IS STATE IN THE MOUNT')
       })
       .catch(err => console.log(err))
   }
@@ -41,36 +40,43 @@ class MyTrips extends React.Component{
         {!this.state.myTrips && <p> Finding trips ...</p>}
         {this.state.myTrips && this.state.myTrips.map(trip => (
           <div key={trip.id} className="mytrip-card">
-            <div className="contains-title">
-              <h1>{trip.name}</h1>
-            </div>
-
-            <div className="images">
-              <img src={trip.images} alt={trip.name}/>
-            </div>
-
-            <div className="contains-budget_duration">
+            <div className="mytrip-title_likes_budget_duration">
+              <div className="mytrip-title">
+                <h1>{trip.name}</h1>
+              </div>
               <div className="budget">
                 <h4>{trip.budget}</h4>
               </div>
               <div className="duration">
                 <h4>{trip.duration.duration}</h4>
               </div>
+              <div className="mytrip-like_viewTrip">
+                <h4>{trip.liked_by.length} likes</h4>
+              </div>
             </div>
 
-            <div className="contains-categories">
-              <h4>{trip.categories.map((category, i) => (
-                <span key={i}>{category.name}, </span>))}</h4>
+            <div className="mytrip-everythingElse">
+              <div className="mytrip-contains-buttons">
+                <button
+                  className="deleteButton"
+                  onClick={() => this.handleDeleteClick(trip)}
+                >delete this trip
+                </button>
+                <Link to={`trips/${trip.id}`}>
+                  <button>view this trip</button> </Link>
+              </div>
+              <div className="mytrip-categories">
+                <h4>{trip.categories.map((category, i) => (
+                  <span key={i}>{category.name}, </span>))}</h4>
+              </div>
+              <div
+                style={{
+                  backgroundImage: `url(${trip.images})`,
+                  backgroundSize: 'cover',
+                  backgroundRepeat: 'noRepeat'
+                }}className="mytrip-images">
+              </div>
             </div>
-            <div className="contains-like_viewTrip">
-              <h4>{trip.liked_by.length} likes</h4>
-            </div>
-            <button
-              className="deleteButton"
-              onClick={() => this.handleDeleteClick(trip)}
-            >Delete this trip
-            </button>
-            <Link to={`trips/${trip.id}`}> <button>VIEW THIS TRIP</button> </Link>
           </div>
         ))}
       </main>
